5d7da058d04781fc69db218617cdb1f6afbb03a3
AbdelZN <54932229+AbdelZN@users.noreply.github.com>
2023-06-07 21:20:27 +0200

    added state management

---
+++ src/stores/userstoresession.js
@@ -0,0 +1,41 @@
+import { defineStore } from "pinia";
+import axios from "../axios-auth";
+
+export const useUserStoreSession = defineStore("usersession", {
+    state: () => ({
+        jwt: '',
+        username: '',
+    }),
+    getters: {
+        isAuthenticated: (state) => {state.jwt != ''},
+    },
+    actions: {
+        locallogin: () => {
+            this.jwt = localStorage.getItem('jwt');
+            this.username = localStorage.getItem('username');
+            axios.defaults.headers.common['Authorization'] = 'Bearer ' + this.jwt;
+        },
+        login: (username, password) => {
+            return new Promise((resolve, reject) => {
+            axios
+                .post('users/login', {
+                    username: username,
+                    password: password
+                })
+                .then(response => {
+                    this.jwt = response.data.jwt;
+                    this.username = response.data.username;
+
+                    localStorage.setItem('jwt', this.jwt);
+                    localStorage.setItem('username', this.username);
+
+                    axios.defaults.headers.common['Authorization'] = 'Bearer ' + this.jwt;
+                    resolve();
+                })
+                .catch(error => {
+                    console.log(error);
+                    reject(error.response.data.errorMessage);
+                });
+            })},
+    }
+})
