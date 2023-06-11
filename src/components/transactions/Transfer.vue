2f75a7439e27308de5cd3ce2547b4f15cf2fee0a
Simona Sofkarovska <675351@student.inholland.nl>
2023-06-11 03:41:21 +0200

    Update Transfer.vue

--- src/components/transactions/Transfer.vue
+++ src/components/transactions/Transfer.vue
@@ -6,13 +6,13 @@
       <div class="card">
         <div class="card-content">
           <div class="row">
-  <div class="col">
-    <label for="pin">PIN:</label>
-  </div>
-  <div class="col">
-    <input type="password" v-model="pin" class="value" />
-  </div>
-</div>
+            <div class="col">
+              <label for="pin">PIN:</label>
+            </div>
+            <div class="col">
+              <input type="password" v-model="pin" class="value" />
+            </div>
+          </div>
           <div class="row">
             <div class="col">
               <label for="remaining-balance">Remaining transfer balance:</label>
@@ -51,7 +51,7 @@
           </div>
         </div>
 
-      
+
         <br><br>
         <div class="transfer-card button">
           <button @click="performTransfer">Transfer</button>
@@ -163,9 +163,11 @@ h2 {
   margin-top: 20px;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
 }
+
 .transfer-card button:hover {
-    background-color: #1a862c;
-  }
+  background-color: #1a862c;
+}
+
 .row {
   display: flex;
   align-items: center;
