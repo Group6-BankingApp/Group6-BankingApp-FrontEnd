dd82fd0457581067be7d4cb311ba54053a57f2be
AbdelZN <54932229+AbdelZN@users.noreply.github.com>
2023-06-15 05:33:21 +0200

    update

---
+++ src/components/transactions/TransactionItem.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h3> {{ transaction.timeCreated }} - {{ transaction.senderIban }} - {{ transaction.receiverIban }} - {{ transaction.amount }}</h3>
+        <br>
+        <hr>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "TransactionItem",
+    props: {
+      transaction: Object
+    }
+  };
+  </script>
+  
+  <style scoped>
+  button {
+    float: right;
+  }
+  h3 {
+    color: #0f642b;
+    text-align: center;
+  }
+  </style>
+  
+  
